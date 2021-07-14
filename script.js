ceea1b6de24dcbf76b9661c97f19a0b1642bbf96
Simon Burg <semburg.sb@gmail.com>
2021-07-14 13:54:01 +0200

    creating the post mail with php, not working from local

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const themeDot = document.getElementsByClassName('theme-dot')
 
+
+// Theme change with save 
+
 let theme = localStorage.getItem('theme')
 
 if (theme == null) {
@@ -9,14 +12,14 @@ if (theme == null) {
 }
 
 for (let i = 0; i < themeDot.length; i++) {
-    themeDot[i].addEventListener('click', function() {
+    themeDot[i].addEventListener('click', function () {
         const mode = this.dataset.mode
         console.log('events working', mode)
         setTheme(mode)
     })
 }
 
-function setTheme(mode){
+function setTheme(mode) {
     if (mode == 'light') {
         document.getElementById('theme-style').href = 'main.css'
     }
@@ -34,4 +37,80 @@ function setTheme(mode){
     }
 
     localStorage.setItem('theme', mode)
+}
+
+// Email Sending form
+
+
+const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', formSend)
+
+
+async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form)
+
+    let formData = new FormData(form)
+
+    if (error===0) {
+        let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+        })
+
+        if (response.ok) {
+           let result = await response.json() 
+           alert(result.message)
+           formPreview.innerHTML = ''
+           form.reset()
+        } else {
+            alert('Error')
+        }
+
+    } else {
+        alert('Form not complete')
+        console.log(error)
+    }
+
+}
+
+
+function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('.input-field')
+
+    for (let i = 0; i < formReq.length; i++) {
+        const element = formReq[i];
+
+        formRemoveError(element)
+
+        if (element.classList.contains('email')) {
+            if (validateEmail(element)) {
+                formAddError(element)
+                error++
+            }
+        } else if (element.value === '') {
+            formAddError(element)
+            error++
+        }
+
+    }
+    return error
+}
+
+function formAddError(input) {
+    input.parentElement.classList.add('_error')
+    input.classList.add('_error')
+}
+
+function formRemoveError(input) {
+    input.parentElement.classList.remove('_error')
+    input.classList.remove('_error')
+}
+
+function validateEmail(email) {
+    return  re = !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
 }
---
+++ sendmail.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('de', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+// from
+$mail->setFrom('burghardt_simon@gmx.de', 'Sem from 1');
+
+// to
+$mail->addAddress('semburg.sb@gmail.com');
+
+// subject
+
+$mail->Subject = "Hi, this is test mail";
+
+
+
+// body
+
+$body = '<h1> Welcome the mail in h1 </h1>'
+
+if (trim(!empty($_POST['email']))) {
+    $body.='<p><strong>E-Mail:<strong> '.$_POST['email'].'</p>';
+}
+
+if (trim(!empty($_POST['message']))) {
+    $body.='<p><strong>E-Mail:<strong> '.$_POST['message'].'</p>';
+}
+
+
+$mail->Body = $body;
+
+if (!$mail->send()) {
+    $message = 'Error sending';
+} else {
+    $message = "Message sent";
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+
+?>
